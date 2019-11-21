60aa5ff328a985c354a714acb6de1b9427e918f2
mihir.m.parekh@gmail.com <mihir_parekh2@cable.comcast.com>
2019-11-21 12:17:39 -0500

    Initial Commit with blank project

---
+++ src/test/java/com/cognizant/course/springrest/restfulwebservice/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.course.springrest.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
