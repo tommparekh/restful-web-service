7436f375dc69050355a37d39e2084bf73c212ba5
tommparekh <mihir.m.parekh@gmail.com>
2019-11-24 17:45:05 -0500

    Custom exception handler added

---
+++ src/main/java/com/cognizant/course/springrest/restfulwebservice/exception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.cognizant.course.springrest.restfulwebservice.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.cognizant.course.springrest.restfulwebservice.user.UserNotFoundException;
+
+
+@RestController
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+		
+		ExceptionResponse exResp = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity(exResp, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) {
+		
+		ExceptionResponse exResp = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(true));
+		
+		return new ResponseEntity(exResp, HttpStatus.NOT_FOUND);
+		
+	}
+
+
+}
